b160ae2f21d15736140cb3759abbed97c70103d8
Chame <chamycr@hotmail.com>
2020-07-11 12:13:45 -0500

    fixed indentation

--- src/main/java/me/zonal/passwordtenshi/MySQL.java
+++ src/main/java/me/zonal/passwordtenshi/MySQL.java
@@ -4,67 +4,67 @@ import java.sql.*;
 
 public class MySQL {
 
-    String dbhost;
-    int dbport;
-    String dbname;
-    String dbuser;
-    String dbpass;
+	String dbhost;
+	int dbport;
+	String dbname;
+	String dbuser;
+	String dbpass;
 
-    public MySQL (final String dbhost2) {
-        this.dbhost = dbhost2;
-    }
-    public void setdbport(final int dbport2) {
-        dbport = dbport2;
-    }
-    public void setdbname(final String dbname2) {
-        dbname = dbname2;
-    }
-    public void setdbuser(final String dbuser2) {
-        dbuser = dbuser2;
-    }
-    public void setdbpass(final String dbpass2) {
-        dbpass = dbpass2;
-    }
+	public MySQL (final String dbhost2) {
+		this.dbhost = dbhost2;
+	}
+	public void setdbport(final int dbport2) {
+		dbport = dbport2;
+	}
+	public void setdbname(final String dbname2) {
+		dbname = dbname2;
+	}
+	public void setdbuser(final String dbuser2) {
+		dbuser = dbuser2;
+	}
+	public void setdbpass(final String dbpass2) {
+		dbpass = dbpass2;
+	}
 
-    public boolean check() {
-        boolean result = false;
-        Connection con = null;
-        ResultSet rs = null;
-        Statement st = null;
-        final String loginip = "jdbc:mysql://"+dbhost+":"+String.valueOf(dbport);
-        final StringBuilder tablereq = new StringBuilder("CREATE TABLE IF NOT EXISTS users (");
-                            tablereq.append("`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,");
-                            tablereq.append("`uuid` VARCHAR(128) NOT NULL,");
-                            tablereq.append("`password` VARCHAR(512) NOT NULL)");
+	public boolean check() {
+		boolean result = false;
+		Connection con = null;
+		ResultSet rs = null;
+		Statement st = null;
+		final String loginip = "jdbc:mysql://"+dbhost+":"+String.valueOf(dbport);
+		final StringBuilder tablereq = new StringBuilder("CREATE TABLE IF NOT EXISTS users (");
+							tablereq.append("`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,");
+							tablereq.append("`uuid` VARCHAR(128) NOT NULL,");
+							tablereq.append("`password` VARCHAR(512) NOT NULL)");
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver"); 
-            con = DriverManager.getConnection(loginip, dbuser, dbpass); 
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); 
+			con = DriverManager.getConnection(loginip, dbuser, dbpass); 
 
-            if(con != null){
-                rs = con.getMetaData().getCatalogs();
-                while(rs.next()){
-                    String catalogs = rs.getString(1);
-                    if(dbname.equals(catalogs)){
-                        result = true;
-                    }
-                }
-            } else{
-                result = false;
-            }
-            
-            con.close();
-            con = DriverManager.getConnection(loginip+"/"+dbname, dbuser, dbpass); 
-            st = con.createStatement();
-            st.executeUpdate(tablereq.toString());
+			if(con != null){
+				rs = con.getMetaData().getCatalogs();
+				while(rs.next()){
+					String catalogs = rs.getString(1);
+					if(dbname.equals(catalogs)){
+						result = true;
+					}
+				}
+			} else{
+				result = false;
+			}
+			
+			con.close();
+			con = DriverManager.getConnection(loginip+"/"+dbname, dbuser, dbpass); 
+			st = con.createStatement();
+			st.executeUpdate(tablereq.toString());
 
-        } catch(Exception ex){
-            ex.printStackTrace();
-            result = false;
+		} catch(Exception ex){
+			ex.printStackTrace();
+			result = false;
 		} finally {
 			if( rs != null){
 				try{
-				    rs.close();
+					rs.close();
 				}
 				catch(SQLException ex){
 					ex.printStackTrace();
@@ -72,46 +72,46 @@ public class MySQL {
 			}
 			if( con != null){
 				try{
-				    con.close();
+					con.close();
 				}
 				catch(SQLException ex){
 					ex.printStackTrace();
 				}
-            }
-        }
-        return result;
-    }
+			}
+		}
+		return result;
+	}
 
-    public boolean addplayer(String playeruuid, String password){
-        boolean result = false;
-        Connection con = null;
-        Statement st = null;
-        final String loginip = "jdbc:mysql://"+dbhost+":"+String.valueOf(dbport)+"/"+dbname;
-        final String insertquery = "INSERT INTO users (uuid, password) VALUES ("+playerid+","+password+")";
+	public boolean addplayer(String playeruuid, String password){
+		boolean result = false;
+		Connection con = null;
+		Statement st = null;
+		final String loginip = "jdbc:mysql://"+dbhost+":"+String.valueOf(dbport)+"/"+dbname;
+		final String insertquery = "INSERT INTO users (uuid, password) VALUES ("+playerid+","+password+")";
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver"); 
-            con = DriverManager.getConnection(loginip, dbuser, dbpass); 
-            if(con != null){
-                st = con.createStatement();
-                st.executeUpdate(insertquery);
-                result = true;
-            }
-            else{
-                result = false;
-            }
-        } catch(Exception ex){
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); 
+			con = DriverManager.getConnection(loginip, dbuser, dbpass); 
+			if(con != null){
+				st = con.createStatement();
+				st.executeUpdate(insertquery);
+				result = true;
+			}
+			else{
+				result = false;
+			}
+		} catch(Exception ex){
 			ex.printStackTrace();
 		} finally {
 			if( con != null){
 				try{
-				    con.close();
+					con.close();
 				}
 				catch(SQLException ex){
 					ex.printStackTrace();
 				}
-            }
-        }
-        return result;
-    }
+			}
+		}
+		return result;
+	}
 } 
