421eb55e8a0ce26f6a2047d7b3124074b4bed0be
Chame <chamycr@hotmail.com>
2020-07-12 22:06:46 -0500

    uuh add another library I forgot

--- src/main/java/me/zonal/passwordtenshi/PasswordTenshi.java
+++ src/main/java/me/zonal/passwordtenshi/PasswordTenshi.java
@@ -7,6 +7,7 @@ import me.zonal.passwordtenshi.commands.CommandRegister;
 import me.zonal.passwordtenshi.commands.CommandUnregister;
 import me.zonal.passwordtenshi.commands.CommandUnregisterPlayer;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.Plugin;
